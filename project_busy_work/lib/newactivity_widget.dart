1f98de351807d5fb85055d794052f205174d521d
Arturo Jimenez <ajimenez2@knox.edu>
2020-04-22 15:57:22 -0500

    New Activity Layout
    Added Layout to New Activity Page

--- project_busy_work/lib/newactivity_widget.dart
+++ project_busy_work/lib/newactivity_widget.dart
@@ -10,13 +10,83 @@ class NewActivityWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('New Activity'),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Home()));
-          },
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text('Title: ', style: TextStyle(fontSize: 24)),
+                  Container(width: 20),
+                  Container(
+                      color: Colors.black,
+                      child:
+                          Text('insert here', style: TextStyle(fontSize: 20)))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Location: ', style: TextStyle(fontSize: 24)),
+                  Container(width: 20),
+                  Container(
+                      color: Colors.black,
+                      child:
+                          Text('insert here', style: TextStyle(fontSize: 20)))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Description: ', style: TextStyle(fontSize: 24)),
+                  Container(width: 20),
+                  Container(
+                      color: Colors.black,
+                      child:
+                          Text('insert here', style: TextStyle(fontSize: 20)))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Date: ', style: TextStyle(fontSize: 24)),
+                  Container(width: 20),
+                  Container(
+                      color: Colors.black,
+                      child:
+                          Text('insert here', style: TextStyle(fontSize: 20)))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Time: ', style: TextStyle(fontSize: 24)),
+                  Container(width: 20),
+                  Container(
+                      color: Colors.black,
+                      child:
+                          Text('insert here', style: TextStyle(fontSize: 20)))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Routine: ', style: TextStyle(fontSize: 24)),
+                  Container(width: 20),
+                  Container(
+                      color: Colors.black,
+                      child:
+                          Text('insert here', style: TextStyle(fontSize: 20)))
+                ],
+              ),
+              Center(
+                child: RaisedButton(
+                  child: Text('New Activity'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home()));
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       backgroundColor: color,
