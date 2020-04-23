9a7840645815456f3cc4e0527dd9ba09090be441
Gaeth <galalou@knox.edu>
2020-04-22 20:36:21 -0700

    Calendar Picker/Bar at the top of overview

--- project_busy_work/lib/home_widget.dart
+++ project_busy_work/lib/home_widget.dart
@@ -15,7 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
   final List<Widget> _children = [
-    OverviewWidget(bgGreen, "Overview"),
+    OverviewWidget(),
     ProgressWidget(bgGreen, "Progress"),
     SettingsWidget(bgGreen, "Settings")
   ];
--- project_busy_work/lib/overview_widget.dart
+++ project_busy_work/lib/overview_widget.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'myColors.dart';
 import 'package:projectbusywork/newactivity_widget.dart';
 
-class OverviewWidget extends StatelessWidget {
-  final Color color;
-  final String text;
-  OverviewWidget(this.color, this.text);
+
+class OverviewWidget extends StatefulWidget{
+  @override 
+    _OverviewState createState() => _OverviewState();
+}
+class _OverviewState extends State<OverviewWidget> {
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
+  CalendarController _controller;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: color,
+      backgroundColor: bgGreen,
       body: SafeArea(
         child: Column(
           children: <Widget>[
             Row(
               children: <Widget>[
                 Container(
-                  //color: Colors.white,
                   width: 320,
                   height: 10,
                 ),
@@ -41,7 +50,42 @@ class OverviewWidget extends StatelessWidget {
                 ),
               ],
             ),
-            Center(child: Text(text)),
+            Container(
+              height: 10,
+              //child: Text("OverView",)
+            ),
+            TableCalendar(
+              calendarController: _controller,
+              initialCalendarFormat: CalendarFormat.week,
+              headerVisible: false,
+              rowHeight:70,
+              daysOfWeekStyle: DaysOfWeekStyle(
+                weekdayStyle: TextStyle(color: Colors.white),
+                weekendStyle: TextStyle(color: hGreen, fontWeight: FontWeight.bold),
+              ),
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events)=>
+                  Container(
+                    child: Text(date.day.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                    margin: EdgeInsets.all(3.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: hGreen,
+                      borderRadius: BorderRadius.circular(25.0),                    
+                    ),
+                  ),
+                dayBuilder: (context, date, events)=>
+                  Container(
+                    child: Text(date.day.toString(), style: TextStyle(color: lightGreen, fontWeight: FontWeight.bold, fontSize: 17),),
+                    margin: EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),                    
+                    ),
+                  ),
+              ),
+            ),
           ],
         ),
       ),
