7c85cf4857bdb9a7b26d1447c579f0c43effd94d
Abraham <aamulualem@knox.edu>
2020-04-23 17:18:02 +0300

    initial commit for AA screen (tasks list)

---
+++ project_busy_work/lib/activity_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'myColors.dart';
+
+class ActivityWidget extends StatefulWidget {
+  @override
+  _ActivityState createState() => _ActivityState();
+}
+
+
+class _ActivityState extends State<ActivityWidget> {
+  final List<ListItem> tasks = [MessageItem('task 1','sleep')];
+  //List<String> tasks = ['task 1', 'task 2', 'task 3'];
+  final TextEditingController eCtrl = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: bgGreen,
+        body: SafeArea(
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+            new Expanded(
+              child:
+              Container(
+                decoration: new BoxDecoration(
+                    color: bgWhite,
+                    borderRadius: new BorderRadius.only(
+                      topLeft: const Radius.circular(40.0),
+                      topRight: const Radius.circular(40.0),
+                      bottomLeft: const Radius.circular(40.0),
+                      bottomRight: const Radius.circular(40.0),
+                    )
+                ),
+                margin:
+                const EdgeInsets.only(left: 20.0, right: 20.0, top: 100, bottom: 300),
+                child:
+              new ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (BuildContext context, int Index) {
+                    return ListTile(
+                      title: tasks[Index].buildTitle(context),
+                      subtitle: tasks[Index].buildSubtitle(context),
+                    );
+
+
+
+                  }
+
+                  //children: tasks.map((task) => Butt(task)).toList(),
+                  ),
+        ),
+            ),
+          ]),
+        ));
+  }
+}
+
+
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String task;
+  final String description;
+
+
+  MessageItem(this.task, this.description);
+
+  Widget buildTitle(BuildContext context) => FloatingActionButton(
+    onPressed: () {},
+    child: Text(task),
+    shape: RoundedRectangleBorder(
+        side: BorderSide(width: 100)),
+    foregroundColor: lGreen,
+    backgroundColor: bgWhite,
+  );
+
+  Widget buildSubtitle(BuildContext context) => Text(description);
+}
--- project_busy_work/lib/home_widget.dart
+++ project_busy_work/lib/home_widget.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:projectbusywork/activity_widget.dart';
 import 'package:projectbusywork/progress_widget.dart';
 import 'package:projectbusywork/settings_widget.dart';
 import 'overview_widget.dart';
@@ -15,14 +16,16 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
   final List<Widget> _children = [
-    OverviewWidget(),
+    //OverviewWidget(),
+
     ProgressWidget(bgGreen, "Progress"),
-    SettingsWidget(bgGreen, "Settings")
+    SettingsWidget(bgGreen, "Settings"),
+    ActivityWidget()
   ];
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       // appBar: AppBar(
       //   title: Text('Busy Work'),
       // ),  commented to remove the app bar
@@ -34,28 +37,23 @@ class _HomeState extends State<Home> {
         height: 50,
         onTap: onTabTapped,
         index: _currentIndex,
-        animationDuration: Duration(
-          milliseconds: 350
-        ),
-        items: <Widget> [
-            Icon(
-              Icons.home, 
-              color: lGreen,
-              size: 25,
-            ),
-
-            Icon(
-              Icons.assessment,
-              color: lGreen,
-              size: 25,
-            ),
-
-            Icon(
-              Icons.settings,
-              color: lGreen,
-              size: 25,
-            ), 
-
+        animationDuration: Duration(milliseconds: 350),
+        items: <Widget>[
+          Icon(
+            Icons.home,
+            color: lGreen,
+            size: 25,
+          ),
+          Icon(
+            Icons.assessment,
+            color: lGreen,
+            size: 25,
+          ),
+          Icon(
+            Icons.settings,
+            color: lGreen,
+            size: 25,
+          ),
         ],
       ),
     );
