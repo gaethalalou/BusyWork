604a222ed6728cc0bc8c84f939b7a2aa3a068126
Gaeth <galalou@knox.edu>
2020-04-28 15:41:59 -0700

    Description Page fixes and clean up
    Adjusted height for the placeholders, also changed the text "description" and added a title to the top

---
+++ project_busy_work/lib/DescriptionPage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'myColors.dart';
+
+class DescriptionPage extends StatefulWidget {
+  @override
+  _DescriptionPage createState() => _DescriptionPage();
+}
+
+class _DescriptionPage extends State<DescriptionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgGreen,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+                children: <Widget>[
+                  IconButton(
+                    alignment: Alignment.topLeft,
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text("Task 1 ", style: TextStyle(fontSize: 20),textAlign: TextAlign.center,)
+                  ),
+                  
+                ], 
+            ),
+            Container(
+              height: 320,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              margin: const EdgeInsets.only(
+                  left: 13.0, right: 13.0, top: 5, bottom: 10),
+            ),
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              margin: const EdgeInsets.only(
+                  left: 13.0, right: 13.0, top: 5, bottom: 10),
+              child: Container(
+                margin: EdgeInsets.only(top:9.0, left: 9.0),
+                child: Text("Description:", textAlign: TextAlign.left, style: TextStyle(color: lGreen, fontWeight: FontWeight.w500, fontSize: 20),)
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
