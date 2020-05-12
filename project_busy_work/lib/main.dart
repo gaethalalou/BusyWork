39509a25a74b5019cbafa19cac3695d0c152b840
Arturo Jimenez <ajimenez2@knox.edu>
2020-05-12 09:19:25 -0500

    Settings Skeleton and Intro Change
    Added some buttons to the settings tab. They don't do anything currently when pressed. Also made it so that the intro screen only shows up when json.tasks is present.

--- project_busy_work/lib/main.dart
+++ project_busy_work/lib/main.dart
@@ -1,22 +1,52 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:projectbusywork/myColors.dart';
 import 'intro.dart';
+import 'package:path_provider/path_provider.dart';
+import 'home_widget.dart';
+import 'placeholder_widget.dart';
 
-void main() { 
+void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
   runApp(App());
 }
 
-class App extends StatelessWidget {
- @override
- 
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Busy Work',
-     theme: ThemeData.dark(),
-     home: IntroScreen(),
-   );
- }
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  Widget first;
+  File jsonFile;
+  Directory dir;
+  String fileName = "tasks.json";
+  bool fileExists = false;
+  List<dynamic> fileContent;
+
+  @override
+  void initState() {
+    first = Home();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonFile = new File(dir.path + "/" + fileName);
+      fileExists = jsonFile.existsSync();
+      if (!fileExists) first = IntroScreen();
+    });
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Busy Work',
+      theme: ThemeData.dark(),
+      home: first,
+    );
+  }
 }
